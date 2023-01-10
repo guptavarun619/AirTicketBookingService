1f2abb6acdd5e7b280b469e68efed20dbae42033
guptavarun619 <guptavarun619@gmail.com>
2023-01-10 23:15:19 +0530

    Added setup for rabbitmq

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,4 +5,7 @@ dotenv.config();
 module.exports = {
   PORT: process.env.PORT,
   FLIGHT_SERVICE_URL: process.env.FLIGHT_SERVICE_URL,
+  MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
+  EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+  NOTIFICATION_BINDING_KEY: process.env.NOTIFICATION_BINDING_KEY,
 };
--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -1,27 +1,39 @@
 const { StatusCodes } = require("http-status-codes");
 const { BookingService } = require("../services/index");
-
+const { createChannel, publishMessage } = require("../utils/messageQueue");
+const { NOTIFICATION_BINDING_KEY } = require("../config/serverConfig");
 const bookingService = new BookingService();
 
-const create = async (req, res) => {
-  try {
-    const response = await bookingService.createBooking(req.body);
-    return res.status(StatusCodes.OK).json({
-      message: "Successfully completed booking",
-      success: true,
-      err: {},
-      data: response,
-    });
-  } catch (error) {
-    return res.status(error.statusCode).json({
-      message: error.message,
-      success: false,
-      err: error.explaination,
-      data: {},
+class BookingController {
+  constructor() {}
+
+  async sendMessageToQueue(req, res) {
+    const channel = await createChannel();
+    const data = { message: "success" };
+    publishMessage(channel, NOTIFICATION_BINDING_KEY.JSON.stringify(data));
+    return res.status(StatusCodes.ok).json({
+      message: "Successfully published the event",
     });
   }
-};
 
-module.exports = {
-  create,
-};
+  async create(req, res) {
+    try {
+      const response = await bookingService.createBooking(req.body);
+      return res.status(StatusCodes.OK).json({
+        message: "Successfully completed booking",
+        success: true,
+        err: {},
+        data: response,
+      });
+    } catch (error) {
+      return res.status(error.statusCode).json({
+        message: error.message,
+        success: false,
+        err: error.explaination,
+        data: {},
+      });
+    }
+  }
+}
+
+module.exports = BookingController;
